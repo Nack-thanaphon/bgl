6c806063bd9cd486b33e6c69af009b065d79eebe
thanaphon <e21bvz@gmail.com>
2024-10-11 01:42:56 +0700

    update

--- public_html/2018/connections/config.inc.new.php
+++ public_html/2018/connections/config.inc.new.php
@@ -1,16 +1,17 @@
 <?php
-
+$db = "news";
 $host = "localhost";
 $user = "aree";
 $password = "Ak@072039";
 
 // Create connection
-$link = mysqli_connect($host, $user, $password);
+$link = mysqli_connect($host, $user, $password, $db);
 
 // Check connection
 if (!$link) {
-	die("Connection failed: " . mysqli_connect_error());
+    die("Connection failed: " . mysqli_connect_error());
 }
 
 // Set character set to UTF-8
 mysqli_query($link, "SET NAMES UTF8");
+?>
