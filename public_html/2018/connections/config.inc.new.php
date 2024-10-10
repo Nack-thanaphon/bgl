d062d3fb36a57d39e994d647601db8eb42b48c3e
thanaphon <e21bvz@gmail.com>
2024-10-11 01:29:49 +0700

    update

--- public_html/2018/connections/config.inc.new.php
+++ public_html/2018/connections/config.inc.new.php
@@ -1,7 +1,4 @@
 <?php
-	$link = mysqli_connect("localhost", "aree", "Ak@072039")or die("Could not connect: " . mysqli_connect_error());
-	mysqli_query($link,"set NAMES UTF8");
-	
-
-
-?>
+$db = "BGL";
+$link = mysqli_connect("localhost", "aree", "Ak@072039", $db) or die("Could not connect: " . mysqli_connect_error());
+mysqli_query($link, "set NAMES UTF8");
